b343a623e0d35adad30fddcc3d7cbea8a0acaf28
John Kang <pollyfactor@hotmail.com>
2020-04-03 19:39:58 -0400

    added route for reading db.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -17,12 +17,35 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// const jsonDb = fs.readFileSync("./db/db.json");
+// const jsonData = JSON.parse(jsonDb);
+// console.log(jsonData);
 
-// app.get("/api/tables", function(req, res) {
-//   res.json(tables);
-// });
+
+app.get("/api/notes", function(req, res) {
+  res.json();
+});
+
+app.get("/api/notes", function(req, res) {
+  // res.json(tables);
+});
+
+app.get("/api/characters/:id", function(req, res) {
+  // var chosen = req.params.id;
+
+  // console.log(chosen);
+
+  // for (var i = 0; i < characters.length; i++) {
+  //   if (chosen === characters[i].routeName) {
+  //     return res.json(characters[i]);
+  //   }
+  // }
+
+  // return res.json(false);
+});
+
 
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on http://localhost:" + PORT);
 });
