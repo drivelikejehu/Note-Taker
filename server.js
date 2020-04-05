fb9923e3d996ae2ba97a2156a91447915e2d45b0
John Kang <pollyfactor@hotmail.com>
2020-04-05 14:48:57 -0400

    updated api/notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const db = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -15,36 +16,15 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// Route to serve index.html
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
 // read the `db.json` file and return all saved notes as JSON
 app.get("/api/notes", function(req, res) {
-  try {
-    let db = fs.readFileSync("db/db.json", "utf8");
-    console.log(db);
-    // parse it so database is an array of objects
-    db = JSON.parse(db);
-    console.log(db);
-
-    // error handling
-  } catch (err) {
-    console.log("You have an error");
-    console.log(err);
-  }
-  //   send objects to the browser
   res.json(db);
 });
 
 app.post("/api/notes", function(req, res) {
-
   try {
-    
-    let dbParsed = JSON.parse(db);
     let idNum = db.length;
-    console.log(idNum);
+    // console.log(idNum);
 
     //receives new note to save and sets to const
     const body = req.body;
@@ -53,7 +33,7 @@ app.post("/api/notes", function(req, res) {
     //adds to db
     db.push(body);
     //stringify db for writefile
-    dbString = JSON.stringify(dbParsed);
+    dbString = JSON.stringify(db);
     
     fs.writeFile("db/db.json", dbString, "utf8", function(err) {
       // error handling
@@ -70,27 +50,54 @@ app.post("/api/notes", function(req, res) {
   
 });
 
+app.delete("/api/notes:id", function(req, res) {
 
-app.get("/api/notes/:id", function(req, res) {
-  const noteid = req.params.id;
+  const id = req.params.id;
+  // const body = req.body;
 
-   fs.readFile("./db/db.json", function(err, data) {
-    if (err) {
-        console.log(err);
-      res.status(500);
-      return res.send("That is not a working title.");
+  for (i = 0; i < db.length; i++) {
+    if (id === db[i].id) {
+
+      const indexOfObject = db.map(function(item) { return item.id; }).indexOf(i);
+      const removed = db.splice(indexOfObject, 1);
+      // tableData.push(req.body);
+      console.log(indexOfObject);
+      console.log(removed);
+      res.send(`Note ${id} has been removed.`)
     }
-    const notesArray = JSON.parse(data);
-
-    for (var i = 0; i < notesArray.length; i++) {
-      if (noteId === notesArray[i].title) {
-        return res.json(notesArray[i]);
-      } else {
-        res.status(404);
-        return res.send("Can't find that note. Please try again.");
+    else {
+      res.status(404).send('Note not found')
     }
-  }
+}
+
 });
+
+
+// app.get("/api/notes/:id", function(req, res) {
+//   const noteid = req.params.id;
+
+//    fs.readFile("./db/db.json", function(err, data) {
+//     if (err) {
+//         console.log(err);
+//       res.status(500);
+//       return res.send("That is not a working title.");
+//     }
+//     const notesArray = JSON.parse(data);
+
+//     for (var i = 0; i < notesArray.length; i++) {
+//       if (noteId === notesArray[i].title) {
+//         return res.json(notesArray[i]);
+//       } else {
+//         res.status(404);
+//         return res.send("Can't find that note. Please try again.");
+//     }
+//   }
+// });
+// });
+
+// Route to serve index.html
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, function() {
