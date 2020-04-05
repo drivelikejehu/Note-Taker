f5b102e69c3b5c69aa83958675e3d0a50ca8fad7
John Kang <pollyfactor@hotmail.com>
2020-04-05 14:18:58 -0400

    updated routes on server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-const jsonDb = fs.readFileSync("./db/db.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,15 +22,54 @@ app.get("*", function(req, res) {
 
 // read the `db.json` file and return all saved notes as JSON
 app.get("/api/notes", function(req, res) {
-  fs.readFile("./db/db.json", function(err, data) {
-        if (err) {
-          res.status(500);
-          return res.send("An error occurred retrieving notes.");
-        }
-        const notesArray = JSON.parse(data);
-        res.json(jsonArray);
-      });
+  try {
+    let db = fs.readFileSync("db/db.json", "utf8");
+    console.log(db);
+    // parse it so database is an array of objects
+    db = JSON.parse(db);
+    console.log(db);
+
+    // error handling
+  } catch (err) {
+    console.log("You have an error");
+    console.log(err);
+  }
+  //   send objects to the browser
+  res.json(db);
 });
+
+app.post("/api/notes", function(req, res) {
+
+  try {
+    
+    let dbParsed = JSON.parse(db);
+    let idNum = db.length;
+    console.log(idNum);
+
+    //receives new note to save and sets to const
+    const body = req.body;
+    //adds an id based on counter
+    Object.assign(body, {id: idNum});
+    //adds to db
+    db.push(body);
+    //stringify db for writefile
+    dbString = JSON.stringify(dbParsed);
+    
+    fs.writeFile("db/db.json", dbString, "utf8", function(err) {
+      // error handling
+      if (err) throw err;
+    });
+    
+    res.json(body);
+
+    // error handling
+  } catch (err) {
+    console.log("You have an error");
+    console.log(err);
+  }
+  
+});
+
 
 app.get("/api/notes/:id", function(req, res) {
   const noteid = req.params.id;
